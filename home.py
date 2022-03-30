ae83fd5e6e5653e91a275c787e520e11024bd447
AndrewC04 <99641772+AndrewC04@users.noreply.github.com>
2022-03-29 19:00:11 -0700

    Update home.py

--- home.py
+++ home.py
@@ -8,19 +8,18 @@ def home():
     name = {'username':'Lisa'}
     city_names = ['Paris','London','Rome','Tahiti']
 
-    return '''
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <h1> Welcome ''' + name['username'] + '''! </h1>
-            <p> <a href="http://www.google.com/"> not google </a> </p>
+    for city in range(len(city_names)):
+        return '''
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <h1> Welcome ''' + name['username'] + '''! </h1>
+                <p> <a href="http://www.google.com/"> not google </a> </p>
             <ul>
-                {% for city in city_names %}
-                <li> {{ city }} </li>
-                {% endfor %}
+                 <li> ''' + city_names[city] + ''' </li>
+                 <li> ''' + city_names[city+1] + ''' </li>
+                 <li> ''' + city_names[city+2] + ''' </li>
+                 <li> ''' + city_names[city+3] + ''' </li>
             </ul>
-        </body>
-    </html> '''
-
-if __name__ == "__main__":
-    myapp_obj.run(debug=True)
+            </body>
+        </html> '''
