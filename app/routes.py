95d6f19c7fc81ac7f1d5f56ed5599c5d27d9a6c1
AndrewC04 <99641772+AndrewC04@users.noreply.github.com>
2022-03-31 23:07:40 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,7 @@
+@myapp_obj.route("/")
 
+def home():
+  name = {'username':'Lisa'}
+  city_names = ['Paris','London','Rome','Tahiti']
+  
+  return render_template('home.html', name=name, cities=city_names)
